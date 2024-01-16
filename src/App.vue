fdb692bbd82f518508cb8cf0164a2d855f1e3ade
Eric Russell <Erictim.Russell@live.com>
2024-01-16 09:02:32 -0700

    inti push

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <header>
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </nav>
+  </header>
+
+  <div>
+    <RouterView />
+  </div>
+</template>
+
+<style scoped>
+header{
+  display: flex;
+  justify-content: center;
+  width: 100%
+}
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+</style>
