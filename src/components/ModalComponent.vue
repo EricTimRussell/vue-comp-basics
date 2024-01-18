4698d5f9e2d68af7b5f529224cfe65042aa0d25e
Eric Russell <Erictim.Russell@live.com>
2024-01-18 10:33:38 -0700

    modal logic seperate component

---
+++ src/components/ModalComponent.vue
@@ -0,0 +1,29 @@
+<template>
+  <Teleport to="body">
+    <div class="modal">
+      <h1>This is a modal</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis eius adipisci ipsam quam beatae placeat
+        dignissimos praesentium totam vel cumque impedit, exercitationem tempore minima? Tempora deserunt temporibus
+        ipsam
+        sint odit?</p>
+      <button>Hide Modal</button>
+    </div>
+  </Teleport>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.modal{
+  background: blue;
+  padding: 10px;
+  border-radius: 5px;
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+}
+</style>
--- src/views/ModalsView.vue
+++ src/views/ModalsView.vue
@@ -2,22 +2,13 @@
   <div class="modals">
     <h1>Modals</h1>
     <button @click="showModal = true">Show Modal</button>
-    <Teleport to="body">
-      <div v-if="showModal" class="modal">
-        <h1>This is a modal</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis eius adipisci ipsam quam beatae placeat
-          dignissimos praesentium totam vel cumque impedit, exercitationem tempore minima? Tempora deserunt temporibus
-          ipsam
-          sint odit?</p>
-        <button @click="showModal = false">Hide Modal</button>
-      </div>
-    </Teleport>
-
+    <ModalComponent v-if="showModal"></ModalComponent>
   </div>
 </template>
 
 <script setup>
 import { ref } from "vue";
+import ModalComponent from "@/components/ModalComponent.vue"
 
 
 const showModal = ref(false)
@@ -25,16 +16,6 @@ const showModal = ref(false)
 </script>
 
 <style scoped>
-.modal{
-  background: blue;
-  padding: 10px;
-  border-radius: 5px;
-  position: absolute;
-  left: 0;
-  top: 0;
-  width: 100%;
-  height: 100%;
-}
 
 
 </style>
